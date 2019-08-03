408eb7a1e01d4e062989dbd97cce33a54d3ce0f0
Fred G <lunarcoffee.pjc@gmail.com>
2019-08-02 22:34:50 -0400

    Add more tests

--- src/test/kotlin/FullTestSuite.kt
+++ src/test/kotlin/FullTestSuite.kt
@@ -2,7 +2,11 @@ import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
-@Suite.SuiteClasses(SubstitutorTests::class)
+@Suite.SuiteClasses(
+    SubstitutorTests::class,
+    BaseConverterTests::class,
+    UnicodeConverterTests::class
+)
 class FullTestSuite {
     companion object {
         const val TEST_RES = "src/test/resources"
---
+++ src/test/kotlin/UnicodeConverterTests.kt
@@ -0,0 +1,10 @@
+import dev.lunarcoffee.malacca.modules.UnicodeConverter
+import org.junit.Test
+
+class UnicodeConverterTests {
+    @Test
+    fun test1() {
+        val result = UnicodeConverter().decode(emptyList(), "129382 32 65 97")
+        assert(result == "\uD83E\uDD66 Aa")
+    }
+}
